caf455253ab57e7de469267e8a8afc10439e3e78
Enrico Franzelli <enrico.franzelli@gmail.com>
2016-04-06 11:31:27 +0200

    Initial Commit

---
+++ Flycatcher/Flycatcher.h
@@ -0,0 +1,19 @@
+//
+//  Flycatcher.h
+//  Flycatcher
+//
+//  Created by Enrico Franzelli on 06/04/16.
+//  Copyright © 2016 Capibara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Flycatcher.
+FOUNDATION_EXPORT double FlycatcherVersionNumber;
+
+//! Project version string for Flycatcher.
+FOUNDATION_EXPORT const unsigned char FlycatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flycatcher/PublicHeader.h>
+
+
